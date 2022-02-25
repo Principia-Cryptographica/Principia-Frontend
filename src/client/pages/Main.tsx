bcb1f1bcad7f57a4a585bb689942f5f838d67bba
Grant Stenger <gstenger98@gmail.com>
2022-02-24 16:36:57 -0800

    adding responsive font scaling for mobile

--- src/client/pages/Main.tsx
+++ src/client/pages/Main.tsx
@@ -8,8 +8,8 @@ const Main = () => {
     <div className="flex bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 items-center flex-col justify-between h-screen">
       <div></div>
       <div className="flex items-center justify-center flex-col align-middle">
-        <h1 className="text-8xl text-white font-eb-garamond">PRINCIPA</h1>
-        <h1 className="text-3xl text-white font-eb-garamond">CRYPTOGRAPHICA</h1>
+        <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl text-white font-eb-garamond">PRINCIPA</h1>
+        <h1 className="text-base sm:text-xl md:text-2xl lg:text-3xl text-white font-eb-garamond">CRYPTOGRAPHICA</h1>
       </div>
       <Footer />
     </div>
